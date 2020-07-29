4bb112445b01cbd2ed67f5ebf8821826740244f6
Putta Khunchalee <putta@zcoin.io>
2020-07-29 13:22:05 +0700

    Allows to override connection string for DesignTimeDbContextFactory

--- src/Zsharp.LightweightIndexer.Postgres/DesignTimeDbContextFactory.cs
+++ src/Zsharp.LightweightIndexer.Postgres/DesignTimeDbContextFactory.cs
@@ -1,12 +1,16 @@
 namespace Zsharp.LightweightIndexer.Postgres
 {
+    using System;
     using Microsoft.EntityFrameworkCore.Design;
 
     public sealed class DesignTimeDbContextFactory : DbContextFactory, IDesignTimeDbContextFactory<DbContext>
     {
         public DesignTimeDbContextFactory()
         {
-            this.ConnectionString = "Host=127.0.0.1;Database=postgres;Username=postgres;Password=postgres";
+            var @override = Environment.GetEnvironmentVariable("ZSHARP_POSTGRES_CONNECTIONSTRING");
+            var @default = "Host=127.0.0.1;Database=postgres;Username=postgres;Password=postgres";
+
+            this.ConnectionString = @override ?? @default;
         }
 
         protected override string ConnectionString { get; }
